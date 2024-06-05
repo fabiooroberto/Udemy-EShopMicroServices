ffdfb3613c4ce3379ba6adbffd7a35e163c0c7ee
Fábio Roberto <fabioo.roberto@outlook.com>
2024-06-04 23:26:35 -0300

    criação buildingBlocks

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, decimal Price, string ImageFile) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            Price = command.Price,
+            ImagemFile = command.ImageFile
+        };
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to container (DI)
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
