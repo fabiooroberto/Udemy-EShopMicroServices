8d0430cac8502b2ac797ba88d11fc1e2394807cd
Fabio Roberto <fabioroberto@wiz.co>
2024-06-11 22:40:41 -0300

    feat(basket) configuracao marten e repository

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,17 +1,19 @@
 ﻿
+using Basket.API.Data;
+
 namespace Basket.API.Basket.GetBasket;
 
 public record GetBasketQuery(string UserName): IQuery<GetBasketResult>;
 
 public record GetBasketResult(ShoppingCart Cart);
 
-public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+public class GetBasketHandler(IBasketRepository _repository) : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
-    public Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
     {
         //TODO: get basket from database
-        //var basket = await _repository.GetBasket(request.UserName);
+        var basket = await _repository.GetBasketAsync(query.UserName);
 
-        return new GetBasketResult(new ShoppingCart("swm"));
+        return new GetBasketResult(basket);
     }
 }
