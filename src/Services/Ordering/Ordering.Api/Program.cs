e406437368e52a17d8694922b42ca96937d95bb2
Fábio Roberto <fabioo.roberto@outlook.com>
2024-06-16 14:59:17 -0300

    estrutura do projeto ordering e dependencyInjection

--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -1,5 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+// Add services to the contaners.
+
+//---------------
+//Infrastructure - EF Core
+//Application - MediatR
+//Api - Carter, HealthChecks, ...
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+//---------------
 var app = builder.Build();
 
+app.UseApiServices();
 
 app.Run();
