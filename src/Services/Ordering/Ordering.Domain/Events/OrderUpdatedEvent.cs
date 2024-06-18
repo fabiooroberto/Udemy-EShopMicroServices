d622361c9a0d13284d0727d6ecb98c58d8111949
Fábio Roberto <fabioo.roberto@outlook.com>
2024-06-17 23:15:19 -0300

    finalização estrutura ordering.domain

---
+++ src/Services/Ordering/Ordering.Domain/Events/OrderUpdatedEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace Ordering.Domain.Events;
+
+public record class OrderUpdatedEvent(Order Order) : IDomainEvent;
--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,12 +1,12 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class Order : Aggretate<Guid>
+public class Order : Aggretate<OrderId>
 {
     private readonly List<OrderItem> _orderItems = new();
     public IReadOnlyCollection<OrderItem> OrderItems => _orderItems.AsReadOnly();
 
-    public Guid Customerid { get; private set; } = default!;
-    public string OrderName { get; private set; } = default!;
+    public CustomerId CustomerId { get; private set; } = default!;
+    public OrderName OrderName { get; private set; } = default!;
     public Address ShippingAdress { get; private set; } = default!;
     public Address BillingAdress { get; private set; } = default!;
     public Payment Payment { get; private set; } = default!;
@@ -15,5 +15,54 @@ public class Order : Aggretate<Guid>
     {
         get => OrderItems.Sum(oi => oi.Price * oi.Quantity);
         private set { }
+    }
+
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAdress, Address billingAdress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAdress = shippingAdress,
+            BillingAdress = billingAdress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
+
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAdress, Address billingAdress, Payment payment, OrderStatus status)
+    {
+        OrderName = orderName;
+        ShippingAdress = shippingAdress;
+        BillingAdress = billingAdress;
+        Payment = payment;
+        Status = status;
+
+        AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+
+        if (orderItem != null)
+        {
+            _orderItems.Remove(orderItem);
+        }
     }
 }
