6a433068917445dea953e44a1c817162669a26ee
Fabio Roberto <fabioroberto@wiz.co>
2024-06-19 09:07:02 -0300

    feat(ordering): criação dos métodos de criar Order

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(o => o.Order.OrderName).NotEmpty().WithMessage("Name is required");
+        RuleFor(o => o.Order.CustomerId).NotNull().WithMessage("CustomerId is required");
+        RuleFor(o => o.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty");
+    }
+}
