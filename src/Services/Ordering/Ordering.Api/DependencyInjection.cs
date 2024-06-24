f5dcc64f9932b7b6d672bcd4fb5e767cfec34eff
Fabio Roberto <fabioroberto@wiz.co>
2024-06-24 10:19:04 -0300

    feat(ordering): criação dos endpoints ordering

--- src/Services/Ordering/Ordering.Api/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -1,15 +1,18 @@
-﻿namespace Ordering.Api;
+﻿using Carter;
+
+namespace Ordering.Api;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
+        services.AddCarter();
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
-        //app.MapCarter();
+        app.MapCarter();
         return app;
     }
 }
--- src/Services/Ordering/Ordering.Api/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Api/GlobalUsing.cs
@@ -1,4 +1,8 @@
-﻿global using Ordering.Application;
-global using Ordering.Infrastructure;
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
 global using Ordering.Api;
+global using Ordering.Application;
+global using Ordering.Application.Dtos;
+global using Ordering.Infrastructure;
 global using Ordering.Infrastructure.Data.Extensions;
